8b268f30b69e4d0e9283e62b94a357088fb7ec5f
nish <nishchalgarg123@gmail.com>
2025-08-18 23:58:25 +0530

    updated

---
+++ weather-app/weather_api.py
@@ -0,0 +1,45 @@
+from typing import Final
+import requests
+import json
+from model import Weather, dt
+
+API_KEY: Final[str] = 'c893739ac9f7a6af3ea35b509023c91c'
+BASE_URL: Final[str] = 'https://api.openweathermap.org/data/2.5/forecast'
+
+def get_weather(city_name: str, mock: bool = True) ->dict:
+    if mock:
+        with open('dummy_data.json')as file:
+            return json.load(file)
+    
+    payload: dict = {'q': city_name, 'appid': API_KEY, 'units': 'metric'}
+    response = requests.get(url=BASE_URL, params=payload)
+    data: dict = response.json()
+    with open('dummy_data.json', 'w') as file:
+        json.dump(data, file)
+    return data
+
+def get_weather_details(weather: dict) -> list[Weather]:
+    days: list[dict] = weather.get('list')
+
+    if not days:
+        raise Exception(f'Problem with json: {weather}')
+    list_of_weather: list[Weather] = []
+    for day in days:
+        w: Weather = Weather(date=dt.fromtimestamp(day.get('dt')),
+                              details=(details := day.get('main')),
+                              temp=details.get('temp'),
+                              weather=(weather := day.get('weather')),
+                              description=weather[0].get('description'))
+        list_of_weather.append(w)
+    return list_of_weather
+
+# if __name__ == '__main__':
+#     current_weather: dict = get_weather('tokyo', mock=True)
+#     weather: list[Weather] = get_weather_details(current_weather)
+#     print("dfcghbm")
+    # print(weather)
+    # for w in weather:
+    #     print(w.temp, w.weather)
+
+
+    
